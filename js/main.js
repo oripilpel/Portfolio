e9f2c04da3caaf03d8013efdb5eb3ddd41ea8660
ori <oripilpelphone@gmail.com>
2021-08-02 21:29:36 +0300

    renderring one proj and almost working modal

--- js/main.js
+++ js/main.js
@@ -5,22 +5,41 @@ function onInit() {
     renderProjects()
 }
 function renderProjects() {
+    console.log(getProjects());
     var html = getProjects().map(function (project) {
-        return `<div class="col-md-4 col-sm-6 portfolio-item">
-        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4">
-        <div class="portfolio-hover">
-            <div class="portfolio-hover-content">
-                <i class="fa fa-plus fa-3x"></i>
+        return `<div class="col-md-4 col-sm-6 portfolio-item"> <a class="portfolio-link" data-toggle="modal" href="#${project.name}"> <div class="portfolio-hover"> <div class="portfolio-hover-content"> <i class="fa fa-plus fa-3x"></i> </div> </div> <img class="img-fluid" src="${project.imgUrl}.png" > </a> <div class="portfolio-caption"> <h4>${project.name}</h4> <p class="text-muted">${project.category}</p> </div>
+        <div class="portfolio-modal modal fade" id=${project.name} tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl"></div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 mx-auto">
+                        <div class="modal-body">
+                            <h2>${project.name}</h2>
+                            <p class="item-intro text-muted">${project.name}</p>
+                            <img class="img-fluid d-block mx-auto" src="${project.imgUrl}-full.png" alt="">
+                                <p>${project.desc}</p>
+                                <ul class="list-inline">
+                                    <li>Date: ${project.publishedAt}</li>
+                                    <li>Category: ${project.category}</li>
+                                </ul>
+                                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                                    <i class="fa fa-times"></i>
+                                    Close Project</button>
+          </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <img class="img-fluid" src="img/portfolio/04-thumbnail.jpg">
-        </a>
-        <div class="portfolio-caption">
-            <h4>Lines</h4>
-            <p class="text-muted">Branding</p>
-        </div> 
-
-        `
-    }).join('')
-    $('projects').html(html)
+    </div>`
+    }).join('');
+    $('.projects').html(html)
 }
+
+
--- js/project-service.js
+++ js/project-service.js
@@ -1,24 +1,29 @@
-var gId = 1;
 var gProjects = []
-var minesweeper = createProject('mineSweeper', 'MineSweeper is a old game most of us know it\'s rules so iv\'e added some features to it to keep things iteresting .. enjoy :)', 'https://oripilpel.github.io/Sprint1/', 1627307451035, ['minesweeper', 'game'])
-console.log(minesweeper);
+var minesweeper = createProject('mineSweeper', 'MineSweeper is a old game most of us know it\'s rules so iv\'e added some features to it to keep things iteresting .. enjoy :)', 'https://oripilpel.github.io/Sprint1/', 1627307451035, ['minesweeper', 'game'], 'game')
+
+addProject(minesweeper)
+
 
 function addProject(project) {
-    getProjects.push(project)
+    gProjects.push(project)
 }
 
-function createProject(name, desc, url, publishedAt, labels) {
+function createProject(name, desc, url, publishedAt, labels, category) {
     return {
-        id=gId++,
+        imgUrl: getImgUrl(name),
         name,
-        title=name.toLowerCase(),
         desc,
         url,
         publishedAt,
-        labels
+        labels,
+        category
     }
 }
 
 function getProjects() {
     return gProjects;
+}
+
+function getImgUrl(projectName) {
+    return `img\\portfolio\\${projectName}`
 }
