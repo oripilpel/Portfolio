bfba86b99007ba125fb831f5ac2857f5def6e490
ori <oripilpelphone@gmail.com>
2021-08-03 09:21:42 +0300

    all working

--- js/main.js
+++ js/main.js
@@ -4,44 +4,62 @@ $(onInit)
 function onInit() {
     renderProjects()
 }
+
 function renderProjects() {
-    console.log(getProjects());
-    var html = getProjects().map(function (project) {
-        return `<div class="col-md-4 col-sm-6 portfolio-item"> <a class="portfolio-link" data-toggle="modal" href="#${project.name}"> <div class="portfolio-hover"> <div class="portfolio-hover-content"> <i class="fa fa-plus fa-3x"></i> </div> </div> <img class="img-fluid" src="${project.imgUrl}.png" > </a> <div class="portfolio-caption"> <h4>${project.name}</h4> <p class="text-muted">${project.category}</p> </div>
-        <div class="portfolio-modal modal fade" id=${project.name} tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="close-modal" data-dismiss="modal">
-                <div class="lr">
-                    <div class="rl"></div>
+    var modalsHtml = ''
+    var projectsHtml = ''
+    getProjects().forEach(function (project) {
+        projectsHtml += `<div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#${project.name}">
+            <div class="portfolio-hover">
+                <div class="portfolio-hover-content">
+                    <i class="fa fa-plus fa-3x"></i>
                 </div>
             </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 mx-auto">
-                        <div class="modal-body">
-                            <h2>${project.name}</h2>
-                            <p class="item-intro text-muted">${project.name}</p>
-                            <img class="img-fluid d-block mx-auto" src="${project.imgUrl}-full.png" alt="">
+            <img class="img-fluid" src="${project.imgUrl}.png">
+        </a>
+        <div class="portfolio-caption">
+            <h4>${project.name}</h4>
+            <p class="text-muted">${project.category}</p>
+        </div>
+    </div>`
+
+        modalsHtml += `<div class="portfolio-modal modal fade" id="${project.name}" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
+                    <div class="lr">
+                        <div class="rl"></div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 mx-auto">
+                            <div class="modal-body">
+                                <h2>${project.name}</h2>
+                                <p class="item-intro text-muted">${project.name}</p>
+                                <img class="img-fluid d-block mx-auto" src="${project.imgUrl}-full.png">
                                 <p>${project.desc}</p>
                                 <ul class="list-inline">
-                                    <li>Created At: ${new Date(project.publishedAt).toLocaleString()}</li>
-                                    <li>Category: ${project.category}</li>
-                                    <li><button class="btn btn-success" data-dismiss="modal" type="button" onclick="window.location.href='${project.url}'">
-                                    Go Play</button></li>
+                                    <li>Created At: ${project.createdAt}</li>
+                                        <li>Category: ${project.category}</li>
+                                        <li><button class="btn btn-success" data-dismiss="modal" type="button"
+                                                onclick="window.location.href='${project.url}'">
+                                                Go Play</button></li>
                                 </ul>
                                 <button class="btn btn-primary" data-dismiss="modal" type="button">
                                     <i class="fa fa-times"></i>
                                     Close Project</button>
-          </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>`
-    }).join('');
-    $('.projects').html(html)
+    });
+    $('.projects').html(projectsHtml)
+    $('.modals-container').html(modalsHtml)
 }
 
 
--- js/project-service.js
+++ js/project-service.js
@@ -1,20 +1,22 @@
 var gProjects = []
-var minesweeper = createProject('mineSweeper', 'MineSweeper is a old game most of us know it\'s rules so iv\'e added some features to it to keep things iteresting .. enjoy :)', 'https://oripilpel.github.io/Sprint1/', 1627307451035, ['minesweeper', 'game'], 'game')
+var minesweeper = createProject('mineSweeper', 'MineSweeper is a old game most of us know it\'s rules so iv\'e added some features to it to keep things iteresting .. enjoy :)', 'https://oripilpel.github.io/Sprint1/', 1627307451035, ['javascript', 'css', 'html'], 'game')
+var touchnums = createProject('touchnums', 'Touch nums is the game you need to touch all the numbers in acending order as fast as you can.. enjoy :)', 'https://oripilpel.github.io/touch-nums/', 1626702651035, ['javascript', 'css', 'html'], 'game')
 
 addProject(minesweeper)
+addProject(touchnums)
 
 
 function addProject(project) {
     gProjects.push(project)
 }
 
-function createProject(name, desc, url, publishedAt, labels, category) {
+function createProject(name, desc, url, createdAt, labels, category) {
     return {
         imgUrl: getImgUrl(name),
         name,
         desc,
         url,
-        publishedAt,
+        createdAt: formatDate(createdAt),
         labels,
         category
     }
@@ -26,4 +28,9 @@ function getProjects() {
 
 function getImgUrl(projectName) {
     return `img\\portfolio\\${projectName}`
+}
+
+function formatDate(timestamp) {
+    var date = new Date(timestamp);
+    return (date.getMonth() + 1) + '/' + date.getFullYear()
 }
